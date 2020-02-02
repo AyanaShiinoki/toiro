ad7ded5f6a0452f6d421ae7a411c08bb586b7cb3
MacUser <macuser@MacUsernoMacBook-Pro.local>
2020-02-02 16:30:13 +0900

    [devise]メール認証機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :lockable
+         :lockable,:confirmable
          # :lockable追記
          # 一定回数ログインをミスするとアカウントをロック
          # 時間経過で解除
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,20 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # # default url
+  # config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 }
+  # mail setting
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => "ayana43.iroha@gmail.com",
+    :password => "voxyedbddyibeebc",
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
