888f2a57720f339eaf009f6e7cc2496d85dae767
MacUser <macuser@MacUsernoMacBook-Pro.local>
2020-02-04 14:13:04 +0900

    [create]ユーザー機能

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -1,2 +1,33 @@
 class Users::UsersController < ApplicationController
+
+	def show
+		@user = User.find (params[:id])
+	end
+
+	def index
+		@users = User.all
+	end
+
+	def edit
+		@user = User.find (params[:id])
+		if @user != current_user
+			redirect_to root_path
+		end
+	end
+
+	def update
+		@user = User.find (params[:id])
+		if @user.update (user_params)
+			redirect_to users_user_path(@user.id)
+		end
+	end
+
+
+
+private
+	def user_params
+		params.require(:user).permit(:name,:user_image,:introduction)
+	end
+
+
 end
