2c41ae5f1194a5fd088d602b4425c21dabdc6c9b
MacUser <macuser@MacUsernoMacBook-Pro.local>
2020-02-06 14:06:51 +0900

    [create]フォロー機能アソシエーション

---
+++ app/controllers/users/relationships_controller.rb
@@ -0,0 +1,2 @@
+class Users::RelationshipsController < ApplicationController
+end
--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,7 @@
 class Relationship < ApplicationRecord
+
+	# アソシエーション
+	belongs_to :follower, class_name: "User"
+	belongs_to :followed, class_name: "User"
+
 end
