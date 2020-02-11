310836e42a8a038711d511b16e96d0a172b56622
MacUser <macuser@MacUsernoMacBook-Pro.local>
2020-02-11 15:12:23 +0900

    [create]管理者側のユーザ詳細、編集画面

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,21 @@ class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
 	def after_sign_in_path_for(resource)
-
 		case resource
-			when Admin
-				admins_root_path
-			when User
-				root_path
-			end
+		when Admin
+		admins_root_path
+		when User
+		root_path
+		end
 	end
 
+	def after_sign_out_path_for(resource_or_scope)
+    	if resource_or_scope == :admin
+      	new_admin_session_path
+    	else
+      	root_path
+    	end
+  end
 
 
 	protected
