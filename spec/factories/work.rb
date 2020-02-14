bbcbaad2aa7f7b8f9f233744826b29c72fee4598
MacUser <macuser@MacUsernoMacBook-Pro.local>
2020-02-14 16:03:55 +0900

    [create]個展のシステムテスト

--- spec/factories/work.rb
+++ spec/factories/work.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :work do
   	work_title { Faker::Lorem.characters(number:10) }
   	description { Faker::Lorem.characters(number:50) }
+  	exhibition nil
   	user
   end
 end
